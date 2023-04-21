bd13f76993113bb400a2f16279eb3ee0bb05111e
bryancasler <bryan.casler@gmail.com>
2023-04-21 17:12:14 -0400

    More adjustments for the inline upsell message

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, April 21, 2023 @ 16:56:44 ET
+ *  Date: Friday, April 21, 2023 @ 17:11:09 ET
  *  By: bryancasler
  *  ENGrid styles: v0.13.56
  *  ENGrid scripts: v0.13.55
