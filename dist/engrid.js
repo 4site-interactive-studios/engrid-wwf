58479383d9ddbdd2ae30842e5c9e7e7edbcc2b34
bryancasler <bryan.casler@gmail.com>
2023-05-15 18:20:51 -0400

    More custom properties

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, May 15, 2023 @ 17:37:45 ET
+ *  Date: Monday, May 15, 2023 @ 18:00:04 ET
  *  By: bryancasler
  *  ENGrid styles: v0.13.69
  *  ENGrid scripts: v0.13.69
