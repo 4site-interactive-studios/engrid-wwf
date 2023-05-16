1bb87a13ffec8a87e0fbbba9d5cb7a717b9bfb96
bryancasler <bryan.casler@gmail.com>
2023-05-16 13:34:58 -0400

    Style the contact blocks form fields to look like normal Inputs and Textareas
    https://www.bugherd.com/projects/341429/tasks/161

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, May 16, 2023 @ 13:05:12 ET
+ *  Date: Tuesday, May 16, 2023 @ 13:33:26 ET
  *  By: bryancasler
  *  ENGrid styles: v0.13.69
  *  ENGrid scripts: v0.13.69
