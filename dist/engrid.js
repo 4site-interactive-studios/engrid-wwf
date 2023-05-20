05f64a114e1c3382f7687d6a758b792ae134081f
bryancasler <bryan.casler@gmail.com>
2023-05-19 20:55:22 -0400

    Better positioning of the premium images across desktop and mobile

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, May 19, 2023 @ 20:49:52 ET
+ *  Date: Friday, May 19, 2023 @ 20:54:13 ET
  *  By: bryancasler
  *  ENGrid styles: v0.13.69
  *  ENGrid scripts: v0.13.69
