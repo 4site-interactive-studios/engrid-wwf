41e6ee43978f151f9fe3474deeff22c2319f7e90
Fernando Santos <fern@ndo.io>
2023-05-22 16:07:55 -0300

    Building ENgrid from Local Again

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import {
-  Options,
-  App,
-  DonationFrequency,
-  DonationAmount,
-} from "@4site/engrid-common"; // Uses ENGrid via NPM
 // import {
 //   Options,
 //   App,
 //   DonationFrequency,
 //   DonationAmount,
-// } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
+// } from "@4site/engrid-common"; // Uses ENGrid via NPM
+import {
+  Options,
+  App,
+  DonationFrequency,
+  DonationAmount,
+} from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 
 import "./sass/main.scss";
 import { customScript } from "./scripts/main";
