cee6d53ac8b53ad9f0510ed3162595335d09efd4
Michael Thomas <michaelthomas372@gmail.com>
2023-09-27 15:47:17 +0200

    show-if-present upstream changes

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, September 21, 2023 @ 09:30:44 ET
+ *  Date: Wednesday, September 27, 2023 @ 09:45:52 ET
  *  By: michael
  *  ENGrid styles: v0.13.74
  *  ENGrid scripts: v0.13.74
@@ -18044,48 +18044,41 @@ class ShowIfPresent {
   }
 
   shouldRun() {
-    //Check if we have any elements on the page that match the pattern for this functionality
-    //e.g. engrid__supporterquestions{id}-present or engrid__supporterquestions{id}-absent
+    // Check if we have any elements on the page that match the pattern for this functionality
+    // e.g. engrid__supporterquestions{id}__{id}-present, etc.
     this.elements = [...document.querySelectorAll('[class*="engrid__supporterquestions"]')].filter(el => {
       const classNames = el.className.split(" ");
-      return classNames.some(className => /^engrid__supporterquestions.*-(present|absent)$/.test(className));
+      return classNames.some(className => /^engrid__supporterquestions\d+(__supporterquestions\d+)*-(present|absent)$/.test(className));
     });
     return this.elements.length > 0;
   }
 
   run() {
-    const actions = []; //Create an array of actions for each element we have
+    const actions = []; // Create an array of actions for each element we have
 
     this.elements.forEach(el => {
-      //mapping to an object with the class name, field name, and type
+      // Mapping to an object with the class name, field name(s), and type
       const classNames = el.className.split(" ");
-      const matchingClass = classNames.find(className => /^engrid__supporterquestions.*-(present|absent)$/.test(className));
+      const matchingClass = classNames.find(className => /^engrid__supporterquestions\d+(__supporterquestions\d+)*-(present|absent)$/.test(className));
       if (!matchingClass) return null;
-      const hyphenIndex = matchingClass.indexOf("-");
-      const fieldId = matchingClass.substring(26, hyphenIndex);
-      const type = matchingClass.substring(hyphenIndex + 1);
+      const typeIndex = matchingClass.lastIndexOf("-");
+      const type = matchingClass.substring(typeIndex + 1);
+      const inputIds = matchingClass.substring(8, typeIndex).split("__").map(id => `supporter.questions.${id.substring(18)}`);
       actions.push({
         class: matchingClass,
-        fieldName: "supporter.questions." + fieldId,
+        fieldNames: inputIds,
         type: type
       });
     }); //Process the actions
 
     actions.forEach(action => {
-      if (!action) return;
-      const input = document.getElementsByName(action.fieldName)[0];
-      const elements = document.querySelectorAll(`.${action.class}`); // If we don't have the input and we have a "-present" element, hide it
+      const inputElements = action.fieldNames.map(fieldName => document.getElementsByName(fieldName)[0]);
+      const elements = document.querySelectorAll(`.${action.class}`);
+      const areAllInputsPresent = inputElements.every(input => !!input);
+      const areAllInputsAbsent = inputElements.every(input => !input); // Hide the elements based on AND conditions
 
-      if (!input && action.type === "present") {
-        this.logger.log(`${action.fieldName} NOT FOUND, hiding elements with class ${action.class}`);
-        elements.forEach(el => {
-          el.style.display = "none";
-        });
-      } // If we do have the input and we have a "-absent" element, hide it.
-
-
-      if (input && action.type === "absent") {
-        this.logger.log(`${action.fieldName} FOUND, hiding elements with class ${action.class}`);
+      if (action.type === "present" && areAllInputsAbsent || action.type === "absent" && areAllInputsPresent) {
+        this.logger.log(`Conditions not met, hiding elements with class ${action.class}`);
         elements.forEach(el => {
           el.style.display = "none";
         });
