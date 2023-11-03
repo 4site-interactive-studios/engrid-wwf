a398519cba524f8100243f8545db02802c4019a8
Michael Thomas <michaelthomas372@gmail.com>
2023-11-03 14:41:07 +0100

    remove bold

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, November 3, 2023 @ 09:35:53 ET
+ *  Date: Friday, November 3, 2023 @ 09:40:17 ET
  *  By: michael
  *  ENGrid styles: v0.15.12
  *  ENGrid scripts: v0.15.13
