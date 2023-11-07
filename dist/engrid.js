2eaa9ca2a8ecd25222b8f493309c4395c3b004a4
Michael Thomas <michaelthomas372@gmail.com>
2023-11-07 12:38:53 +0100

    remove italics from amount nudge

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, November 3, 2023 @ 09:44:15 ET
+ *  Date: Tuesday, November 7, 2023 @ 06:37:36 ET
  *  By: michael
  *  ENGrid styles: v0.15.12
  *  ENGrid scripts: v0.15.13
