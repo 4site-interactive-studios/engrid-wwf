6975b7c1a092f210d7fb645deaaea45cf3a1a8aa
bryancasler <bryan.casler@gmail.com>
2023-12-15 21:57:50 -0600

    Rebuild

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,10 +17,10 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, December 14, 2023 @ 13:48:57 ET
- *  By: michael
+ *  Date: Friday, December 15, 2023 @ 22:57:00 ET
+ *  By: bryancasler
  *  ENGrid styles: v0.16.4
- *  ENGrid scripts: v0.16.7
+ *  ENGrid scripts: v0.16.8
  *
  *  Created by 4Site Studios
  *  Come work with us or join our team, we would love to hear from you
@@ -13748,6 +13748,9 @@ class CreditCard {
         };
         if (!this.ccField)
             return;
+        // Set credit card field to type="tel" to prevent mobile browsers from
+        //  showing a credit card number keyboard
+        this.ccField.type = "tel";
         const expireFiels = document.getElementsByName("transaction.ccexpire");
         if (expireFiels) {
             this.field_expiration_month = expireFiels[0];
@@ -21380,7 +21383,7 @@ class ENValidators {
 }
 
 ;// CONCATENATED MODULE: ./node_modules/@4site/engrid-common/dist/version.js
-const AppVersion = "0.16.7";
+const AppVersion = "0.16.8";
 
 ;// CONCATENATED MODULE: ./node_modules/@4site/engrid-common/dist/index.js
  // Runs first so it can change the DOM markup before any markup dependent code fires
