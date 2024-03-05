30922780e03e0011488996a8788fa849a4981d3d
Fernando Santos <fern@ndo.io>
2024-03-05 18:32:02 -0300

    Setting VGS Height

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, March 5, 2024 @ 14:53:13 ET
+ *  Date: Tuesday, March 5, 2024 @ 16:31:42 ET
  *  By: fernando
  *  ENGrid styles: v0.17.19
  *  ENGrid scripts: v0.17.20
