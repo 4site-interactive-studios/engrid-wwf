93b310cfc4570e07a25070999a25095694bce1fc
Michael Thomas <michaelthomas372@gmail.com>
2024-09-17 17:47:18 +0200

    add styles for content header

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, September 17, 2024 @ 08:57:04 ET
+ *  Date: Tuesday, September 17, 2024 @ 11:46:31 ET
  *  By: michael
  *  ENGrid styles: v0.18.19
  *  ENGrid scripts: v0.18.19
