7e12f5489f9f59d0aee3f9a9002bed41a8ed57d5
npgiano-macbook <npgiano@gmail.com>
2025-10-03 14:04:10 -0400

    Styling 2026 Footer

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,8 +17,8 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, October 3, 2025 @ 02:26:35 ET
- *  By: fernando
+ *  Date: Friday, October 3, 2025 @ 13:56:42 ET
+ *  By: npgiano
  *  ENGrid styles: v0.22.18
  *  ENGrid scripts: v0.22.19
  *
