bb8140d7e6f3c8902edaa108c8bc80537123e8d0
Fernando Santos <fern@ndo.io>
2025-10-15 15:15:45 -0400

    Implementing Minimal Header Feature

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Wednesday, October 15, 2025 @ 14:55:06 ET
+ *  Date: Wednesday, October 15, 2025 @ 15:15:07 ET
  *  By: fernando
  *  ENGrid styles: v0.22.18
  *  ENGrid scripts: v0.22.20
