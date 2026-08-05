46de031ec6cf66b3146d81194831d376db5ee420
npgiano <nick@stone.money>
2026-08-05 15:47:14 -0400

    rebuild for proper attribute assignment

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Wednesday, September 2, 2026 @ 12:38:40 ET
+ *  Date: Wednesday, September 2, 2026 @ 12:38:53 ET
  *  By: nick
  *  ENGrid styles: v0.28.3
  *  ENGrid scripts: v0.28.3
