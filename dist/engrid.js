71a1ec05ac8fd7c0c308bdf5fc6161b8fd43ce3a
npgiano <nick@stone.money>
2026-08-27 13:50:15 -0400

    Use main's Firefox approach for premium radios: native input with transform scale(1.5)

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, August 27, 2026 @ 13:42:38 ET
+ *  Date: Thursday, August 27, 2026 @ 13:49:58 ET
  *  By: nick
  *  ENGrid styles: v0.27.3
  *  ENGrid scripts: v0.27.6
