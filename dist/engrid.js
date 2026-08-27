e473c4f266c77a106b81adf494cf9906194c0d54
npgiano <nick@stone.money>
2026-08-27 13:43:24 -0400

    Fix premium gift radio scaling on Gecko: set both radio dimensions in Firefox hack override

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, August 27, 2026 @ 13:24:21 ET
+ *  Date: Thursday, August 27, 2026 @ 13:42:38 ET
  *  By: nick
  *  ENGrid styles: v0.27.3
  *  ENGrid scripts: v0.27.6
