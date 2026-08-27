c14e5149153048df16a3278d41bc862e86ae2c6e
npgiano <nick@stone.money>
2026-08-27 14:14:43 -0400

    Firefox: use custom ::before premium radios (appearance:none) instead of scaled native input

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, August 27, 2026 @ 13:49:58 ET
+ *  Date: Thursday, August 27, 2026 @ 14:14:21 ET
  *  By: nick
  *  ENGrid styles: v0.27.3
  *  ENGrid scripts: v0.27.6
